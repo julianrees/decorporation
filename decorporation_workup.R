aa52f81812c97214ea0f806637115a1f5d6cf7b2
sheaos <sheaos@lbl.gov>
2017-06-16 11:44:06 -0700

    hm

--- decorporation_workup.R
+++ decorporation_workup.R
@@ -1,1 +1,39 @@
 # this is our first R script in decorporation
+
+#library(rJava)
+#library(xlsx)
+library(ggplot2)
+library(reshape2)
+
+weights <- read.csv(file = "Data/decorporation536 weights.csv")
+
+class(weights)
+dim(weights)
+plot(weights)
+weight1 <- weights [1:60, 2]
+weight2 <- weights [1:60, 3]
+plot(weight1)
+plot(weight2)
+
+groups <- matrix(LETTERS[1:15])
+groups <- cbind(groups, groups, groups, groups)
+groups <- as.vector(t(groups))
+weights <- cbind(groups, weights)
+colnames(weights) <- c("group", "number", "day.0.weight", "day.2.weight")
+ggplot(weights, aes(x=group, y=day.0.weight)) + 
+  geom_point()
+
+mweights <- melt(weights, id= c("group", "number"))
+ggplot(mweights, aes(x=group, y=value))+
+  geom_boxplot(aes(fill=group))+
+  facet_wrap(~variable)
+
+ggplot(mweights, aes(x=variable, y=value))+
+  geom_boxplot(aes(fill=variable))+
+  facet_wrap(~group)
+
+weights <- cbind(weights, weights$day.2.weight-weights$day.0.weight)
+colnames(weights) <- c(colnames(weights[1:4]), "change")
+
+ggplot(weights, aes(x=groups, y=change))+
+  geom_boxplot(aes(fill=groups))
