aaf3332003f28c09cdaecd17d8bca2bb03f0bc5f
sheaos <sheaos@lbl.gov>
2017-06-16 15:58:59 -0700

    tried to assign arms failed hmmmmmm

--- decorporation_workup.R
+++ decorporation_workup.R
@@ -4,6 +4,7 @@
 #library(xlsx)
 library(ggplot2)
 library(reshape2)
+library(plyr)
 
 weights <- read.csv(file = "Data/decorporation536 weights.csv")
 
@@ -15,6 +16,8 @@ weights <- read.csv(file = "Data/decorporation536 weights.csv")
 #plot(weight1)
 #plot(weight2)
 
+# --- MAKING COLUMNS ----
+
 groups <- matrix(LETTERS[1:15])
 groups <- cbind(groups, groups, groups, groups)
 groups <- as.vector(t(groups))
@@ -25,10 +28,17 @@ colnames(weights) <- c("group", "number", "day.0.weight", "day.2.weight")
 
 mweights <- melt(weights, id= c("group", "number"))
 
-weights <- cbind(weights, weights$day.2.weight-weights$day.0.weight)
-colnames(weights) <- c(colnames(weights[1:4]), "change")
+weights <- cbind(weights, weights$day.2.weight-weights$day.0.weight, (weights$day.2.weight/weights$day.0.weight)*100)
+colnames(weights) <- c(colnames(weights[1:4]), "change", "percent")
 
 
+#as.quoted(c("control", "DOTA/TREN", "Scn")) 
+#ddply(.data=weights, .variables = (control, DOTA/TREN, Scn), .fun = mutate)
+
+#arm <- ddply(weights, .(group), mutate)
+
+#ddply(mexp[ which(mexp$ExpNum == i), ]
+weights[ which(weights$group == "A"), ]
 
 #--- BEGIN PLOTTING OF DATA ----
 
@@ -38,13 +48,28 @@ fhei = 6
 theme_set(theme_bw())
 theme_update(plot.title = element_text(hjust = 0.5))
 
+# --- WEIGHTS BY DAY AND GROUP ----
+
 ggplot(mweights, aes(x=group, y=value))+
   geom_boxplot(aes(fill=group))+
   facet_wrap(~variable)
 
 ggplot(mweights, aes(x=variable, y=value))+
   geom_boxplot(aes(fill=variable))+
-  facet_wrap(~group)
+  facet_wrap(~group)+
+  scale_fill_brewer()
+
+# --- GROSS WEIGHT CHANGE ----
 
 ggplot(weights, aes(x=groups, y=change))+
   geom_boxplot(aes(fill=groups))
+
+# --- PERCENT WEIGHT CHANGE ----
+
+ggplot(weights, aes(x=groups, y=percent))+
+  geom_boxplot(aes(fill=groups))
+
+ggplot(weights, aes(x=groups, y=percent))+
+  geom_point(aes(color=groups))+
+  scale_color_brewer(palette = "Spectral")
+  
